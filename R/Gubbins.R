33e1008696b637de1bb90e94391ca9aa2fc8ff2c
ClairBee <clair.with.no.e@gmail.com>
2016-07-04 14:52:57 +0100

    hijack function corrected (typo on Rbloggers version)

--- R/Gubbins.R
+++ R/Gubbins.R
@@ -140,7 +140,7 @@ hijack <- function (FUN, ...) {
     .FUN <- FUN
     args <- list(...)
     invisible(lapply(seq_along(args), function(i) {
-        formals(.FUN)[[names(args)[i]]] <- args[[i]]
+        formals(.FUN)[[names(args)[i]]] <<- args[[i]]
     }))
     .FUN
 }
