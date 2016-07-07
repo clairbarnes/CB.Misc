63202d336c9ee86184db1a3479897a7b418b20ca
ClairBee <clair.with.no.e@gmail.com>
2016-07-07 15:50:52 +0100

    Image.scale function added (taken wholesale from Rbloggers)

--- R/Gubbins.R
+++ R/Gubbins.R
@@ -129,7 +129,7 @@ panel.cor <- function(x, y, digits = 2, cex.cor = 1, ...)
 
 #' Hijack a function to change default parameters
 #' 
-#' Create copy function with defaults set to something more useful
+#' Create copy function with defaults set to something more useful. Taken from r-bloggers somewhere
 #' @export
 #' @examples
 #' .data.frame <- hijack(data.frame, stringsAsFactors = FALSE)
@@ -143,4 +143,44 @@ hijack <- function (FUN, ...) {
         formals(.FUN)[[names(args)[i]]] <<- args[[i]]
     }))
     .FUN
+}
+
+
+#' Scale bar for image plot
+#' 
+#' Draw scale bar for image plot. Taken from http://menugget.blogspot.co.uk/2011/08/adding-scale-to-image-plot.html#more
+#' @export
+image.scale <- function(z, zlim, col = heat.colors(12),
+                        breaks, horiz=TRUE, ylim=NULL, xlim=NULL, ...){
+    if(!missing(breaks)){
+        if(length(breaks) != (length(col)+1)){stop("must have one more break than colour")}
+    }
+    if(missing(breaks) & !missing(zlim)){
+        breaks <- seq(zlim[1], zlim[2], length.out=(length(col)+1)) 
+    }
+    if(missing(breaks) & missing(zlim)){
+        zlim <- range(z, na.rm=TRUE)
+        zlim[2] <- zlim[2]+c(zlim[2]-zlim[1])*(1E-3)#adds a bit to the range in both directions
+        zlim[1] <- zlim[1]-c(zlim[2]-zlim[1])*(1E-3)
+        breaks <- seq(zlim[1], zlim[2], length.out=(length(col)+1))
+    }
+    poly <- vector(mode="list", length(col))
+    for(i in seq(poly)){
+        poly[[i]] <- c(breaks[i], breaks[i+1], breaks[i+1], breaks[i])
+    }
+    xaxt <- ifelse(horiz, "s", "n")
+    yaxt <- ifelse(horiz, "n", "s")
+    if(horiz){YLIM<-c(0,1); XLIM<-range(breaks)}
+    if(!horiz){YLIM<-range(breaks); XLIM<-c(0,1)}
+    if(missing(xlim)) xlim=XLIM
+    if(missing(ylim)) ylim=YLIM
+    plot(1,1,t="n",ylim=ylim, xlim=xlim, xaxt=xaxt, yaxt=yaxt, xaxs="i", yaxs="i", ...)  
+    for(i in seq(poly)){
+        if(horiz){
+            polygon(poly[[i]], c(0,0,1,1), col=col[i], border=NA)
+        }
+        if(!horiz){
+            polygon(c(0,0,1,1), poly[[i]], col=col[i], border=NA)
+        }
+    }
 }
