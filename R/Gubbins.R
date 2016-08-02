207354e4722f4fc3bf4942d918d9bf2b6063e387
ClairBee <clair.with.no.e@gmail.com>
2016-08-02 18:07:25 +0100

    ColRamp plotting and image scale; also generic wrapper function to prep output image

--- R/Gubbins.R
+++ R/Gubbins.R
@@ -166,3 +166,73 @@ clump.centres <- function(px) {
                 r = ceiling(max(max(x) - min(x), max(y) - min(y)) / 2))
     df
 }
+
+
+#' Draw test square showing colour ramp
+#' 
+#' Test function: plot a test square, shaded according to a given colour ramp.
+#' @export
+#' 
+test.colramp <- function(mycolramp, n = 100) {
+    z = matrix(1:n, nrow = 1)
+    x = 1
+    y = seq(3, 2345, len = n) # supposing 3 and 2345 are the range of your data
+    
+    image(x, y, z, col = mycolramp(n), axes = FALSE, xlab = "", ylab = "")
+}
+
+
+#' open image plot, ready to output from later commands
+#' 
+#' @export
+#' 
+#' @examples
+#' cb.out("jpeg", "./tmp")
+#' plot(which(pw.m[,,"black", "loan"] > 10000, arr.ind = T))
+#' dev.off()
+#' 
+cb.out <- function(fformat, filenm, mar = c(2,2,1,1), ...) {
+    
+    ffun <- eval(parse(text = fformat))
+    ffun(paste0(filenm, ".", fformat), ...)
+    par(mar = mar)
+}
+
+
+#' plot separate image scale
+#'
+#' Plot rectangular image scale, based on given values & colours
+#' @export
+#' 
+image.scale <- function(z, zlim, col = heat.colors(12),
+                        breaks, axis.pos=1, add.axis=TRUE, ...){
+    if(!missing(breaks)){
+        if(length(breaks) != (length(col)+1)){stop("must have one more break than colour")}
+    }
+    if(missing(breaks) & !missing(zlim)){
+        breaks <- seq(zlim[1], zlim[2], length.out=(length(col)+1)) 
+    }
+    if(missing(breaks) & missing(zlim)){
+        zlim <- range(z, na.rm=TRUE)
+        zlim[2] <- zlim[2]+c(zlim[2]-zlim[1])*(1E-3)#adds a bit to the range in both directions
+        zlim[1] <- zlim[1]-c(zlim[2]-zlim[1])*(1E-3)
+        breaks <- seq(zlim[1], zlim[2], length.out=(length(col)+1))
+    }
+    poly <- vector(mode="list", length(col))
+    for(i in seq(poly)){
+        poly[[i]] <- c(breaks[i], breaks[i+1], breaks[i+1], breaks[i])
+    }
+    if(axis.pos %in% c(1,3)){ylim<-c(0,1); xlim<-range(breaks)}
+    if(axis.pos %in% c(2,4)){ylim<-range(breaks); xlim<-c(0,1)}
+    plot(1,1,t="n",ylim=ylim, xlim=xlim, axes=FALSE, xlab="", ylab="", xaxs="i", yaxs="i", ...)  
+    for(i in seq(poly)){
+        if(axis.pos %in% c(1,3)){
+            polygon(poly[[i]], c(0,0,1,1), col=col[i], border=NA)
+        }
+        if(axis.pos %in% c(2,4)){
+            polygon(c(0,0,1,1), poly[[i]], col=col[i], border=NA)
+        }
+    }
+    box()
+    if(add.axis) {axis(axis.pos)}
+}
