20695afade7b25021c2c4dade7f80016bd75f360
ClairBee <clair.with.no.e@gmail.com>
2019-11-22 10:29:02 +0000

    List songs missing from phone

--- R/system.R
+++ R/system.R
@@ -16,3 +16,25 @@ change.wallpaper <- function() {
 }
 
 
+
+#' Compare music on laptop with music on phone, list any missing tracks
+#' 
+#' @ export
+#' 
+new.songs.to.add.to.phone <- function() {
+    
+    mpath <- "/run/user/1000/gvfs/'mtp:host=%5Busb%3A001%2C006%5D/Internal shared storage'/Music"
+    spath <- "/run/user/1000/gvfs/'mtp:host=%5Busb%3A001%2C006%5D/Internal shared storage'/syncr"
+    
+    # list recursively, append / to folders then exclude
+    on.phone <- system(paste("ls -R -p ", mpath, " | grep -v /"), intern = T)
+    from.syncr <- system(paste("ls -R -p ", spath, " | grep -v /"), intern = T)
+    
+    on.laptop <- list.files("~/Music", recursive = T)
+    on.laptop <- on.laptop[-grep("\\.zip", on.laptop)]
+    
+    files.on.laptop <- basename(on.laptop)
+    
+    on.laptop[!files.on.laptop %in% c(on.phone, from.syncr)]
+    
+}
