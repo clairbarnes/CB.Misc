6a34015e52fe06fa2e2389021aa4f14c1f23a6a0
clairbarnes <clair.with.no.e@gmail.com>
2021-05-30 15:58:31 +0100

    lower case for wallpapers directory

--- R/system.R
+++ R/system.R
@@ -6,12 +6,12 @@
 #'
 change.wallpaper <- function() {
     fnm <- system("gsettings get org.gnome.desktop.background picture-uri", intern = T)
-    fnm <- gsub("'", "", gsub("file:///home/clair/Pictures/Wallpapers/", "", fnm))
-    fl <- list.files("/home/clair/Pictures/Wallpapers/")
+    fnm <- gsub("'", "", gsub("file:///home/clair/Pictures/wallpapers/", "", fnm))
+    fl <- list.files("/home/clair/Pictures/wallpapers/")
     fl <- fl[-which(fl == fnm)]
 
-    new.fnm <- sample(list.files("/home/clair/Pictures/Wallpapers/"), 1)
-    system(paste0("gsettings set org.gnome.desktop.background picture-uri file:///home/clair/Pictures/Wallpapers/",
+    new.fnm <- sample(list.files("/home/clair/Pictures/wallpapers/"), 1)
+    system(paste0("gsettings set org.gnome.desktop.background picture-uri file:///home/clair/Pictures/wallpapers/",
                   new.fnm))
 }
 
